b0a985eea062747ea612f99da8fdd32b02f9122b
Zachary McManus <zacharymcmanus@gmail.com>
2019-09-06 16:24:05 -0700

    udated config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/img`,
       },
     },
     `gatsby-transformer-sharp`,
